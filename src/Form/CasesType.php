bd8a222578279df2157116fc20f800d35b8d85f4
nhadi19 <nelsonhadi1@gmail.com>
2020-05-22 11:36:58 +0200

    Modification des labels dans la page de création de plateau

--- src/Form/CasesType.php
+++ src/Form/CasesType.php
@@ -6,13 +6,14 @@ use App\Entity\Cases;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CasesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('descriptifDefi')
+            ->add('descriptifDefi',TextType::class, ['attr' => ['placeholder' => "Saisir un descriptif"]])
             //->add('consignes')
             //->add('codeValidation')
             //->add('numero')
