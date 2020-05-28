581f6752847e91fe2cc8557d057f1ef42d59e76e
nhadi19 <nelsonhadi1@gmail.com>
2020-05-28 15:37:32 +0200

    Modification de la mise en forme du formulaire

--- src/Form/CasesType.php
+++ src/Form/CasesType.php
@@ -7,16 +7,25 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class CasesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('descriptifDefi',TextType::class, ['attr' => ['placeholder' => "Saisir un descriptif"]])
+            ->add('descriptifDefi',Textareatype::class, ['attr' => ['placeholder' => "Saisir un descriptif"], 'label' => false])
             //->add('consignes')
             //->add('codeValidation')
             //->add('numero')
+            ->add('position', HiddenType::class, [
+            'attr' => [
+                'class' => 'position-case',
+                      ],
+              ])
+
+
         ;
     }
 
--- src/Form/PlateauType.php
+++ src/Form/PlateauType.php
@@ -37,36 +37,13 @@ class PlateauType extends AbstractType
                                                             'Moyen' => 'Moyen',
                                                             'Difficile' => 'Difficile'
                                                         ]])
-            ->add('nbCases', ChoiceType::class, ['choices'  => [
-                                                            ' '  => 1,
-                                                            '12' => 12,
-                                                            '13' => 13,
-                                                            '14' => 14,
-                                                            '15' => 15,
-                                                            '16' => 16,
-                                                            '17' => 17,
-                                                            '18' => 18,
-                                                            '19' => 19,
-                                                            '20' => 20,
-                                                            '21' => 21,
-                                                            '22' => 22,
-                                                            '22' => 22,
-                                                            '23' => 23,
-                                                            '24' => 24,
-                                                            '25' => 25,
-                                                            '26' => 26,
-                                                            '27' => 27,
-                                                            '28' => 28,
-                                                            '29' => 29,
-                                                            '30' => 30,
-                                                            '31' => 31
-                                                        ]])
             ->add('cases', CollectionType::class,
                  ['entry_type' => CasesType::class,
                   'entry_options' => ['label' => false],
                   'allow_add' => true,
                   'allow_delete' => true,
                   'by_reference' => false,
+                  'label'=> false,
                   'prototype' => true,
                   'attr'         => [
                    'class' => 'collection-cases',
