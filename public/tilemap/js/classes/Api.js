2239a8781bbb50bd2f5eb30eceaf43a192c8362f
nhadi19 <nelsonhadi1@gmail.com>
2020-06-17 18:32:45 +0200

    Refactoring js api pour changer plus facilement l'adresse de l'url

--- public/tilemap/js/classes/Api.js
+++ public/tilemap/js/classes/Api.js
@@ -1,27 +1,9 @@
 class Api {
-  static getSmthSync() {
-    var parametres = 'http://localhost:8000/api/partie/' + idPartie;
 
-    // Création de l'objet XmlHttpRequest
-    var xhr = getXMLHttpRequest();
-
-    // Chargement du fichier
-    xhr.open("GET", parametres, false);
-    xhr.send(null);
-    if (xhr.readyState != 4 || (xhr.status != 200 && xhr.status != 0)){// Code == 0 en local
-      throw new Error("Impossible de charger la carte nommée \"" + nom + "\" (code HTTP : " + xhr.status + ").");
-    }
-
-    // Récupération des données
-    var mapJsonData = xhr.responseText;
-    //Parsing
-    var mapData = JSON.parse(mapJsonData);
-    return mapData;
-  }
 
   static postPositionPionSync(pion) {
 
-    var url = 'http://localhost:8000/api/partie/' + idPartie;
+    var url = urlPartie + idPartie;
 
     // Création de l'objet XmlHttpRequest
     var xhr = getXMLHttpRequest();
@@ -44,31 +26,9 @@ class Api {
     xhr.send("$data=" + jsonString);
   }
 
-  static getSmthAsync(url) {
-
-    var xhr = new XMLHttpRequest();
-
-    xhr.addEventListener('readystatechange', function() { // On gère ici une requête asynchrone
-
-      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) { // Si le fichier est chargé sans erreur
-
-        return this.responseText;
-
-      }else if (xhr.readyState === XMLHttpRequest.DONE && xhr.status != 200) {
-        alert('Une erreur est survenue ! !\n\nCode :' + xhr.status + '\nTexte : ' + xhr.statusText);
-      }
-
-    });
-
-    // On souhaite juste récupérer le contenu du fichier, la méthode GET suffit amplement :
-    xhr.open('GET', url);
-
-    xhr.send(null); // La requête est prête, on envoie tout !
-  }
-
   static getParametresPartieJSON(partieId) {
 
-    var UrlApiPartie = 'http://localhost:8000/api/partie/' + partieId;
+    var UrlApiPartie = urlPartie + partieId;
 
     // Création de l'objet XmlHttpRequest
     var xhr = getXMLHttpRequest();
